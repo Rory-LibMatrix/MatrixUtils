b9a08ec061f271df57d0a14667fe6af3d523db6b
Rory& <root@rory.gay>
2024-01-24 12:18:32 +0100

    Removal of download links due to maintenance burden

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,13 +12,3 @@ rm -rf **/bin/Release
 cd MatrixRoomUtils.Web
 dotnet publish -c Release
 rsync -raP bin/Release/net8.0/publish/wwwroot/ rory.gay:/data/nginx/html_mru/
-cd bin/Release/net8.0/publish/wwwroot
-tar cf - ./ | xz -z -9 - > $BASE_DIR/MRU-BIN.tar.xz
-#rsync -raP $BASE_DIR/MRU-BIN.tar.xz rory.gay:/data/nginx/html_mru/MRU-BIN.tar.xz
-rm -rf $BASE_DIR/MRU-BIN.tar.xz
-cd $BASE_DIR
-git clone .git -b `git branch --show-current` src --recursive
-rm -rf src/.git
-tar cf - src/ | xz -z -9 - > MRU-SRC.tar.xz
-rsync -raP $BASE_DIR/MRU-SRC.tar.xz rory.gay:/data/nginx/html_mru/MRU-SRC.tar.xz
-rm -rf src/

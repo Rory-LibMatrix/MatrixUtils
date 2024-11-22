4601c2f1fcdaf551705a9da3c5e6ddfc3eff9138
Rory& <root@rory.gay>
2024-11-22 06:42:27 +0100

    Some fixes all over, policy list work

--- MatrixUtils.Web/Program.cs
+++ MatrixUtils.Web/Program.cs
@@ -8,6 +8,8 @@ using MatrixUtils.Web;
 using MatrixUtils.Web.Classes;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SpawnDev.BlazorJS;
+using SpawnDev.BlazorJS.WebWorkers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -16,6 +18,18 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 // builder.Logging.SetMinimumLevel(LogLevel.Trace);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazorJSRuntime();
+builder.Services.AddWebWorkerService(webWorkerService =>
+{
+    // Optionally configure the WebWorkerService service before it is used
+    // Default WebWorkerService.TaskPool settings: PoolSize = 0, MaxPoolSize = 1, AutoGrow = true
+    // Below sets TaskPool max size to 2. By default the TaskPool size will grow as needed up to the max pool size.
+    // Setting max pool size to -1 will set it to the value of navigator.hardwareConcurrency
+    webWorkerService.TaskPool.MaxPoolSize = 2;
+    // Below is telling the WebWorkerService TaskPool to set the initial size to 2 if running in a Window scope and 0 otherwise
+    // This starts up 2 WebWorkers to handle TaskPool tasks as needed
+    webWorkerService.TaskPool.PoolSize = webWorkerService.GlobalScope == GlobalScope.Window ? 2 : 0;
+});
 
 try {
     builder.Configuration.AddJsonStream(await new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }.GetStreamAsync("/appsettings.json"));
@@ -65,4 +79,5 @@ builder.Services.AddScoped<TieredStorageService>(x =>
 
 builder.Services.AddRoryLibMatrixServices();
 builder.Services.AddScoped<RMUStorageWrapper>();
-await builder.Build().RunAsync();
+// await builder.Build().RunAsync();
+await builder.Build().BlazorJSRunAsync();
